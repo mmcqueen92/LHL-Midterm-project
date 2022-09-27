c073ceae7ebcb82a3cb92afa07aebd6f306d8403
Stanley Surjanto <stan.surjanto@gmail.com>
2022-09-27 16:06:28 -0600

    able to query and render cards with correct info to container

--- db/queries/getAllCards.js
+++ db/queries/getAllCards.js
@@ -3,8 +3,8 @@ const db = require('../connection');
 const getAllCards = () => {
   return db.query('SELECT * FROM cards;')
     .then(data => {
-      console.log(data.rows[0]);
-      return data.rows[0];
+      console.log(data.rows);
+      return data.rows;
     });
 };
 
--- routes/cards-api.js
+++ routes/cards-api.js
@@ -7,9 +7,9 @@ router.get('/', (req, res) => {
     console.log('reg', req);
     console.log('res', res);
     cardQuery.getAllCards()
-    .then((cardInfo) => {
-        // passsing in the cardInfo as res json object here to app.js
-        res.json(cardInfo)
+    .then((cardTileInfo) => {
+        // passsing in the cardTileInfo as res json object here to app.js
+        res.json(cardTileInfo)
     })
 })
 

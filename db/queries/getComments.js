dce117e5b642f294dd1a3ec5bfe83821fe64b8e3
Stanley Surjanto <stan.surjanto@gmail.com>
2022-09-28 17:29:39 -0600

    queries and creates correct comment boxes prepended to the comments container

--- db/queries/getComments.js
+++ db/queries/getComments.js
@@ -1,12 +1,19 @@
 const db = require('../connection');
 
 const getComments = (card_id) => {
+  const queryString = `
+    SELECT comments.*, users.name
+    FROM comments
+    JOIN users ON comments.user_id = users.id
+    WHERE comments.card_id = $1;
+  `;
   // Store this as a $1,2,3 template value to protect against sql injections
   const values = [card_id];
-  return db.query(`SELECT * FROM comments WHERE card_id = $1;`, values)
+  return db.query(queryString, values)
   .then((res) => {
     return res.rows;
   })
 };
 
 module.exports = { getComments };
+

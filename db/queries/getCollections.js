f23113bfde35fb55d7bc34bf96743da238e55e50
Stanley Surjanto <stan.surjanto@gmail.com>
2022-09-29 17:02:30 -0600

    changed query to just give back collection names

--- db/queries/getCollections.js
+++ db/queries/getCollections.js
@@ -4,12 +4,9 @@ const getCollections = (user_id) => {
   // Store this as a $1,2,3 template value to protect against sql injections
   const values = [user_id];
   const queryString = `
-    SELECT collections.user_id AS collection_owner, collections.*, cards.user_id AS card_owner, cards.*
+    SELECT name
     FROM collections
-    JOIN cards_collections ON collection_id = collections.id
-    JOIN cards ON cards_collections.card_id = cards.id
-    WHERE collections.user_id = 4
-    GROUP BY collections.id, cards.id;
+    WHERE collections.user_id = $1;
   `;
 
   return db.query(queryString, values)
@@ -19,3 +16,12 @@ const getCollections = (user_id) => {
 };
 
 module.exports = { getCollections };
+
+// THIS GETS US: all collection names, all cards in those collections (regardless who made them) for CURRENT user from /login/:userid
+
+// SELECT collections.user_id AS collection_owner, collections.*, cards.user_id AS card_owner, cards.*
+// FROM collections
+// JOIN cards_collections ON collection_id = collections.id
+// JOIN cards ON cards_collections.card_id = cards.id
+// WHERE collections.user_id = 4
+// GROUP BY collections.id, cards.id;
